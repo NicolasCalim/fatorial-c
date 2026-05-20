bb89a03dcc561ed624753ee9c1d5519211ed44ff
NicolasCalim <nclimas11@gmail.com>
2026-05-20 19:08:56 -0300

    fatorial.c

--- fatorial.c
+++ fatorial.c
@@ -5,7 +5,7 @@
 int main(int argc, const char *argv[]) {
 	if (argc != 2)
 	{
-	printf("digite o numero que deseja fatorar: ");
+	printf("Digite o nome do programa seguido do numero na qual deseja fatorar.");
 	}
 
 	else {
